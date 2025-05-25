f5d010c586d1886ba7db3090d2d6b2df86653ea9
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-26 01:13:25 +0530

    Update 04_create_task.sql

--- sql/04_create_task.sql
+++ sql/04_create_task.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE TASK monitoring.detect_new_tables_task
-  WAREHOUSE = your_warehouse
+  WAREHOUSE = COMPUTE_WH
   SCHEDULE = 'USING CRON 30 7 * * * UTC'  -- Runs daily at 1:00 PM IST
 AS
   CALL monitoring.detect_new_tables_all_schemas();
