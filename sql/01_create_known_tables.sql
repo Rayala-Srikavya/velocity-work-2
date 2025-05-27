1cc182df2d269471a5f024e70481bce6290c1ea6
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-27 17:47:31 +0530

    Update 01_create_known_tables.sql

--- sql/01_create_known_tables.sql
+++ sql/01_create_known_tables.sql
@@ -6,3 +6,14 @@ CREATE TABLE IF NOT EXISTS monitoring.known_tables (
     created_at TIMESTAMP
 );
 
+-- Insert existing tables only if known_tables is empty
+INSERT INTO monitoring.known_tables (table_schema, table_name, created_at)
+SELECT t.table_schema, t.table_name, t.created
+FROM information_schema.tables t
+WHERE t.table_catalog = CURRENT_DATABASE()
+  AND t.table_schema NOT IN ('INFORMATION_SCHEMA', 'MONITORING')
+  AND NOT EXISTS (
+      SELECT 1
+      FROM monitoring.known_tables k
+      WHERE k.table_schema = t.table_schema AND k.table_name = t.table_name
+  );
