2dd5f4ef047bcbaf9fa025a66b33c105463f6c9c
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-28 20:51:40 +0530

    Update 00_bootstrap_known_tables.sql

--- sql/sql/00_bootstrap_known_tables.sql
+++ sql/sql/00_bootstrap_known_tables.sql
@@ -1,5 +1,5 @@
--- This should only be run ONCE manually or at first deploy
-INSERT INTO config.known_tables (table_schema, table_name, created_at)
+-- Run only once to populate known_tables the first time
+INSERT INTO CONFIG.known_tables (table_schema, table_name, created_at)
 SELECT t.table_schema, t.table_name, t.created
 FROM information_schema.tables t
 WHERE t.table_catalog = CURRENT_DATABASE()
