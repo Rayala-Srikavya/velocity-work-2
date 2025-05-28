e4259de9320072b82effe33a7cc0ecbfd6a65653
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-28 23:40:57 +0530

    Update 04_create_task.sql

--- sql/04_create_task.sql
+++ sql/04_create_task.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE TASK CONFIG.detect_new_tables_task
+CREATE OR REPLACE TASK CONFIG.detect_new_tables_velocity_task
   WAREHOUSE = BUSINESS_ANALYTICS_WH
   SCHEDULE = 'USING CRON 30 7 * * * UTC'
 AS
-  CALL CONFIG.detect_new_tables_all_schemas();
+  CALL CONFIG.velocity_detect_new_tables_all_schemas();
