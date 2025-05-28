33a3e1e11158a756108498e4e0c440788ea410be
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-28 20:50:00 +0530

    Update 01_create_known_tables.sql

--- sql/01_create_known_tables.sql
+++ sql/01_create_known_tables.sql
@@ -1,9 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS CONFIG;
 
-CREATE TABLE IF NOT EXISTS config.known_tables (
+CREATE TABLE IF NOT EXISTS CONFIG.known_tables (
     table_schema STRING,
     table_name STRING,
     created_at TIMESTAMP
 );
-
-
