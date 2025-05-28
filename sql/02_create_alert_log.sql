76cda36d8c048cf49fd700f693bae118f1018663
Rayala-Srikavya <142518414+Rayala-Srikavya@users.noreply.github.com>
2025-05-28 20:50:20 +0530

    Update 02_create_alert_log.sql

--- sql/02_create_alert_log.sql
+++ sql/02_create_alert_log.sql
@@ -1,8 +1,7 @@
-CREATE TABLE IF NOT EXISTS config.alert_log (
+CREATE TABLE IF NOT EXISTS CONFIG.alert_log (
     event_time TIMESTAMP,
     schema_name STRING,
     table_name STRING,
     created_at TIMESTAMP,
     message STRING
 );
-
